5f3cf51e604ea9ec2af3b6ac904c0410b430deae
xuxinyigege <34591641+xuxinyigege@users.noreply.github.com>
2017-12-17 10:55:08 +0800

    Add files via upload

--- family_try2/main.cpp
+++ family_try2/main.cpp
@@ -1,15 +1,44 @@
-#include <QApplication>
+
+
 #include "mainmenu.h"
-#include <QMessageBox>
+
+#include<QMessageBox>
+#include<QApplication>
 #include <QSqlDatabase>
-#include <QPushButton>
+#include<QPushButton>
 
-int main(int argc, char* argv[])
-{
-    QApplication a(argc, argv);
 
-    MainMenu w;
-    w.show();
+// main//
+int main(int argc, char* argv[]){
+    QApplication a(argc,argv);
+    QSqlDatabase db=QSqlDatabase::addDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("member.db");
+    if(!db.open())
+    {
+        QMessageBox::critical(0,"Cannot open database1","Unable to establish a database connection.",QMessageBox::Cancel);
+        return false;
+    }
+    else ui->status_Label->setText(tr("创建sqlite数据库成功"));
 
-    return a.exec();
+    QSqlQuery query;
+    bool success=query.exec("create table member (familyname varchar(40) primary key,name varchar(40),gender varchar(40),fathername varchar(40),islive varchar(40),address varchar(100),telnumber varchar(40),generation int,");
+       if (success){
+           qDebug()<<QObject::tr("数据库表创建成功");
+           return true;
+        }
+       else {
+           qDebug()<<QObject::tr("failed");
+           return false;
+       }
+       if(db.open())
+       {
+          MainMenu w;
+          w.show();
+          return a.exec();
+       }
+       else
+      {
+          QMessageBox::information(NULL,"提示","系统载入数据库失败，无法运行",QMessageBox::Yes);
+      }
+
 }
